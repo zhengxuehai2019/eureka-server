1eeedb7d0baa4558706b5a73f061ce3d8bffa44f
publicclasszxh@sina.com <publicclasszxh@sina.com>
2020-08-06 20:44:43 +0800

    first----

--- src/main/java/com/example/eurekaserver/invercation/ImplSubject.java
+++ src/main/java/com/example/eurekaserver/invercation/ImplSubject.java
@@ -3,6 +3,6 @@ package com.example.eurekaserver.invercation;
 public class ImplSubject implements Subject {
     @Override
     public void doSome() {
-        System.out.println("做了一些事情~~~");
+        System.out.println("做了一些事情~~~first");
     }
 }
